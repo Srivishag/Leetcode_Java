a8e2b698d2b08a34f1731225a6b3e637c6cb54b1
Srivishag P <91311329+Srivishag@users.noreply.github.com>
2024-09-07 18:42:03 +0530

    Create getRow.java

---
+++ getRow.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> arr=new ArrayList<>();
+        long last=1;
+        arr.add(1);
+        //int val=1
+        for(int i=1;i<rowIndex+1;i++){
+            long current=(last*(rowIndex-i+1)/i);
+            arr.add((int)current);
+            last=current;
+        }
+        return arr;
+    }
+}
